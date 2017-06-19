c759214f144ed16f6cdd84357a22c268ca4dffab
Pat Sukhum <patsukhum@172-13-12-16.lightspeed.wchtks.sbcglobal.net>
2017-06-19 14:06:06 -0400

    Restaurant List half done

--- src/app.js
+++ src/app.js
@@ -12,21 +12,31 @@ import {
   View
 } from 'react-native';
 import { Router, Scene, Actions } from 'react-native-router-flux';
+import { Icon } from 'native-base';
+
 import LoginPage from './components/LoginPage';
 import HomePage from './components/HomePage';
 
 
 
 class App extends Component {
+  renderNavBar(){
+    return (
+      <View style={{backgroundColor:"blue"}}>
+        <Text>Test</Text>
+      </View>
+    );
+  }
+
   render() {
     return (
     <Router>
+
+      <Scene key="root">
+        <Scene key="HomePage" component={HomePage} type="reset" title="EcoBites" titleStyle={{fontWeight:'bold', fontSize: 20}} navigationBarStyle={{backgroundColor:"red"}} renderBackButton={()=> <Icon name='menu' />}  renderTitle={this.renderNavBar.bind(this)}/>
+      </Scene>
       <Scene key="Login">
         <Scene key="LoginPage" component={LoginPage}  title={"EcoBites"} titleStyle={{fontWeight:'bold', fontSize: 20, color: "white"}} navigationBarStyle={{backgroundColor:'rgba(52,52,52,0)',borderBottomWidth:0}} />
-      </Scene>
-      <Scene key="root">
-        <Scene key="HomePage" component={HomePage} type="reset" title="EcoBites" titleStyle={{fontWeight:'bold', fontSize: 20}} navigationBarStyle={{backgroundColor:"#F8F8F9"}}  />
-
       </Scene>
     </Router>
     );
